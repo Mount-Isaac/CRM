a9c39607ad674d35f12ec8f9638b3d880cc5d09c
Mount-Isaac <kyalomutie@students.ku.ac.ke>
2023-05-04 02:02:35 +0300

    changed model.py

--- project/crm_app/models.py
+++ project/crm_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class crm_model(models.Model):
+	def __init__(self):
+		self.model_name = "testing github push"
+	def __str__(self):
+		return f'{self.model_name.capitalize()}'
